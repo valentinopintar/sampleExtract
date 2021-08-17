180d38e327839a79c895e813e0f8e81b9e867f91
Valentino <45625518+valentinopintar@users.noreply.github.com>
2021-08-17 13:26:25 +0200

    Update sampleExtractwithR.R

--- sampleExtractwithR.R
+++ sampleExtractwithR.R
@@ -9,10 +9,10 @@ head(speciesData, 10)
 speciesName <- 'Population'                                # headers data for species name
 xy <- species[,c("Longitude","Latitude")]                  # xy location
 envVar <- list.files(pattern=".tif$", full.names = TRUE)   # environmental variables (pattern = .asc, .tif, .grd...)
-rasterStack <- stack(envVar)                               # a set of rasters with the same spatial range and resolution 
+rasterStack <- raster::stack(envVar)                               # a set of rasters with the same spatial range and resolution 
 valueExtract <- extract(rasterStack, xy)                   # extract values from rasters
 envSample <- cbind(speciesData, valueExtract)              # combining (merging) the source table and the obtained values 
 head(envSample, 10)
 
 write.csv2(envSample, file = "Species_envSample.csv")      # write .csv file
-write.xlsx(envSample, file = "Species_envSample.xlsx")     # write excel file
+openxlsx::write.xlsx(envSample, file = "Species_envSample.xlsx")     # write excel file
